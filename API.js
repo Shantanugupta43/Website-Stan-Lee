a894ec0fd7d468ef9e6f4021a70080d624ade2b0
Shantanu Gupta <99300527+Shantanugupta43@users.noreply.github.com>
2022-03-20 20:31:02 +0000

    Change made in API

--- API.js
+++ API.js
@@ -124,3 +124,15 @@ return data.json();
     document.getElementById('api-for-character-captain').innerHTML=captain;
     document.getElementById('api-for-description-captain').innerHTML=captain_des;
 });
+
+fetch('https://gateway.marvel.com/v1/public/characters?nameStartsWith=f&limit=100&ts=1&apikey=15a342a6687d10453e0e9a634cf66302&hash=1aa81eca0614de5bd80a3567e2e7e73f').then((data)=>{
+
+return data.json();
+}).then((completedata)=>{
+    char_name1 = JSON.stringify(completedata.data.results[1].name);
+    falcon = JSON.parse(char_name1);
+    char_des1 = JSON.stringify(completedata.data.results[1].description);
+    falcon_des = JSON.parse(char_des1);
+    document.getElementById('api-for-character-falcon').innerHTML=falcon;
+    document.getElementById('api-for-description-falcon').innerHTML=falcon_des;
+});
